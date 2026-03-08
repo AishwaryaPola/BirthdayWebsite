31899ecc4041e018e1da9552898004784c0cbf2c
AishwaryaPola <paishwarya2003@gmail.com>
2026-03-08 22:19:37 +0530

    added party popups

--- birthday-website/src/app/pages/greeting/greeting.spec.ts
+++ birthday-website/src/app/pages/greeting/greeting.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Greeting } from './greeting';
+import { GreetingComponent } from './greeting';
 
-describe('Greeting', () => {
-  let component: Greeting;
-  let fixture: ComponentFixture<Greeting>;
+describe('GreetingComponent', () => {
+  let component: GreetingComponent;
+  let fixture: ComponentFixture<GreetingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Greeting],
+      imports: [GreetingComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Greeting);
+    fixture = TestBed.createComponent(GreetingComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
