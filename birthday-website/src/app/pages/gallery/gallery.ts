c290ca617cdccddee28d082fd197b7b9f2317990
AishwaryaPola <paishwarya2003@gmail.com>
2026-03-14 18:30:05 +0530

    added pictures

--- birthday-website/src/app/pages/gallery/gallery.ts
+++ birthday-website/src/app/pages/gallery/gallery.ts
@@ -12,55 +12,106 @@ export class GalleryComponent {
 
   memories = [
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic1.jpeg',
       description: 'The cutest little version of you 🧸',
       date: 'Childhood'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic2.jpeg',
+      description: 'The very 1st outing of us✨',
+      date: "September 24, 2022"
+    },
+    {
+      image: 'assets/images/pic3.jpeg',
+      description: 'Hemro level look',
+      date: 'one of the finest day in 2022'
+    },
+    {
+      image: 'assets/images/pic4.jpeg',
+      description: 'A picture that always makes me smile',
+      date: 'January 25, 2023'
+    },
+    {
+      image: 'assets/images/pic5.jpeg',
+      description: 'Even then you were adorable',
+      date: 'Childhood'
+    },
+  
+    {
+      image: 'assets/images/pic6.jpeg',
       description: 'Your smile makes everything brighter ✨',
       date: 'A beautiful day'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic7.png',
       description: 'One of my favourite moments with you ❤️',
       date: 'Together'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic8.png',
       description: 'A picture that always makes me smile',
       date: 'Lovely moment'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic9.png',
       description: 'Even then you were adorable',
       date: 'Childhood'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic10.png',
+      description: 'One of my favourite moments with you ❤️',
+      date: 'Together'
+    },
+    {
+      image: 'assets/images/pic11.png',
+      description: 'A picture that always makes me smile',
+      date: 'Lovely moment'
+    },
+    {
+      image: 'assets/images/pic12.png',
+      description: 'Even then you were adorable',
+      date: 'Childhood'
+    },
+     {
+      image: 'assets/images/pic13.png',
       description: 'The cutest little version of you 🧸',
       date: 'Childhood'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic14.png',
       description: 'Your smile makes everything brighter ✨',
       date: 'A beautiful day'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic15.png',
       description: 'One of my favourite moments with you ❤️',
       date: 'Together'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic16.png',
       description: 'A picture that always makes me smile',
       date: 'Lovely moment'
     },
     {
-      image: 'https://images.unsplash.com/photo-1627639679638-8485316a4b21?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGtpZHxlbnwwfHwwfHx8MA%3D%3D',
+      image: 'assets/images/pic17.png',
       description: 'Even then you were adorable',
       date: 'Childhood'
-    }
+    },
+     {
+      image: 'assets/images/pic18.png',
+      description: 'Even then you were adorable',
+      date: 'Childhood'
+    },
+     {
+      image: 'assets/images/pic19.png',
+      description: 'Even then you were adorable',
+      date: 'Childhood'
+    },
+ {
+      image: 'assets/images/pic20.jpeg',
+      description: 'Even then you were adorable',
+      date: 'Childhood'
+    },
     
   ];
 selectedImage:string | null = null;
@@ -72,4 +123,6 @@ openImage(img:string){
 closeImage(){
   this.selectedImage = null;
 }
+
+
 }
